38ebcb88a606a4cad1a9bf6d51c73e72721b3cd5
Anisa_Ismail <164259547+AnisaIsmail@users.noreply.github.com>
2025-02-02 19:38:00 +0500

    Add files via upload

---
+++ final-hackathon/next.config.ts
@@ -0,0 +1,14 @@
+
+/** @type {import('next').NextConfig} */
+export default {
+  reactStrictMode: true,
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'cdn.sanity.io',
+          },
+],
+unoptimized: true,     
+  },
+};
---
+++ final-hackathon/src/app/page.tsx
@@ -0,0 +1,19 @@
+import SProduct from "./components/furniture"
+import HeroSection from "./components/hero"
+
+
+
+ export default function Home (){
+    return (
+      <div>
+       
+      <HeroSection/>
+      
+        <SProduct />
+      
+      </div>
+    )
+ }
+
+
+   
